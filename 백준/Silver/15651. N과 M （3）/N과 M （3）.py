51c2b4857990864c1387740666ded795afc5f74f
SeungHyun Lee <93538221+222SeungHyun@users.noreply.github.com>
2022-09-21 15:07:32 +0900

    [Silver III] Title: N과 M (3), Time: 1536 ms, Memory: 30840 KB -BaekjoonHub

---
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.py
@@ -0,0 +1,17 @@
+def dfs():
+    if len(arr) == m:
+        print(' '.join(map(str, arr)))
+        return
+    for i in range(1, n+1):
+        visited[i] = True
+        arr.append(i) 
+        dfs()
+        arr.pop()
+        visited[i] = False
+
+
+n, m = map(int, input().split())
+arr = []
+visited = [False for _ in range(n+1)]
+
+dfs()
